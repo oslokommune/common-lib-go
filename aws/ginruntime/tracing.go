5c1e5e2f524b061562092b1e5d07c74df64484af
Eirik Vesterkjær <eirik.vesterkjar@origo.oslo.kommune.no>
2025-01-29 09:41:33 +0100

    Logging hooks (#58)
    - rydd opp i logger for responser som feiler
    - logging hooks

--- aws/ginruntime/tracing.go
+++ aws/ginruntime/tracing.go
@@ -3,12 +3,15 @@ package ginruntime
 import (
 	"net/http"
 
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda/xrayconfig"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/contrib/propagators/aws/xray"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
 )
 
 func (e *GinEngine) oTelLambdaOptions() []otellambda.Option {
@@ -53,4 +56,51 @@ func (e *GinEngine) useHttpServerSpanMiddleware(service string) {
 	}
 
 	e.Use(otelgin.Middleware(service, e.otelGinOptions(service)...))
+}
+
+// Extracts trace ID from the logging context and writes it to the `X-Amzn-Trace-Id` key in the log event.
+//
+// Usage:
+//
+//	log.Logger = log.Logger.Hook(ginruntime.XAmznTraceIdLoggerHook{})
+type XAmznTraceIdLoggerHook struct{}
+
+const xAmznTraceIdHeader = "X-Amzn-Trace-Id"
+
+func (h XAmznTraceIdLoggerHook) Run(e *zerolog.Event, level zerolog.Level, msg string) {
+	if e.GetCtx() == nil {
+		return
+	}
+
+	carrier := propagation.MapCarrier{}
+	xray.Propagator{}.Inject(e.GetCtx(), &carrier)
+
+	if traceId, ok := carrier[xAmznTraceIdHeader]; ok {
+		e.Str(xAmznTraceIdHeader, traceId)
+	}
+}
+
+const datadogTraceIdKey = "dd.trace_id"
+const datadogSpanIdKey = "dd.span_id"
+
+// Extracts trace ID and span ID from the logging context and writes it to the `dd.trace_id` and `dd.span_id` keys in the log event.
+//
+// Usage:
+//
+//	log.Logger = log.Logger.Hook(ginruntime.DatadogTraceCorrelationLoggerHook{})
+//
+// Reference: https://docs.datadoghq.com/logs/log_configuration/pipelines/?tab=traceid#trace-id-attribute
+type DatadogTraceCorrelationLoggerHook struct{}
+
+func (h DatadogTraceCorrelationLoggerHook) Run(e *zerolog.Event, level zerolog.Level, msg string) {
+	if e.GetCtx() == nil {
+		return
+	}
+
+	spanCtx := trace.SpanContextFromContext(e.GetCtx())
+
+	if spanCtx.IsValid() {
+		e.Str(datadogTraceIdKey, spanCtx.TraceID().String())
+		e.Str(datadogSpanIdKey, spanCtx.SpanID().String())
+	}
 }
--- aws/go.mod
+++ aws/go.mod
@@ -1,6 +1,6 @@
 module github.com/oslokommune/common-lib-go/aws
 
-go 1.22
+go 1.23
 
 require (
 	github.com/aws/aws-sdk-go-v2 v1.32.3
